2e48c1c6825be1c66b45561e84d1dd3e41262425
Klaudiusz <d1f1.6@protonmail.com>
2022-05-03 12:38:17 +0200

    OpenAPI implemented - Swagger

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -7,11 +7,19 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('ProjectX API')
+    .setDescription('The NestJS ProjectX API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
